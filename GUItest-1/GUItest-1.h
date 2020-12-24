e4657922a5e43fb0ce08d0c81d4296e0c0673c42
仕旻 张 <lljkiff@163.com>
2020-12-24 13:18:41 +0800

    Delete
    删除一些不必要的文件

--- GUItest-1/GUItest-1.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-#include "framework.h"
-
-#include "Resource.h"		//内含GUI组件UID定义
-#include "Defines.h"		//内含对Win32API GUIclass命名的定义
-#include "ErrorResp.h"		//内含错误处理函数
-#include "MsgAction.h"		//内含各窗口/控件的响应程式
